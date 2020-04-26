bfd912bb480d49283322b64f875f40d878ab2383
impactlearner <time.darden@gmail.com>
2020-04-26 19:19:34 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,9 @@
 # Write your code here
+happy_new_year = 0;
 count = 10
-while count >= 1 do
+when 10 >= count >= 1 do
   puts count
   count -= count
+when count = 0;
+  puts "Happy New Year!"
 end
